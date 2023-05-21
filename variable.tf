bcf21779666581deba136f7588f9b682f8bd1af9
Jonas Lilja <jonas@lilja.io>
2023-05-21 23:10:52 +0200

    Added rest of files

---
+++ variable.tf
@@ -0,0 +1,20 @@
+variable "namecheap_user_name" {
+  type        = string
+  description = "(Required) A registered user name for Namecheap"
+}
+
+variable "namecheap_api_user" {
+  type        = string
+  description = "(Required) A registered api user for Namecheap"
+}
+
+variable "namecheap_api_key" {
+  type        = string
+  description = "(Required) The Namecheap API key"
+}
+
+variable "namecheap_domain" {
+  description = "The Namecheap domain to configure"
+  type        = string
+  default     = "lilja.io"
+}
