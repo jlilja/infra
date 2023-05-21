a7f9d4cfbd5b04cef7b5d190dac78d98200d20c3
Jonas Lilja <jonas@lilja.io>
2023-05-21 22:51:46 +0200

    Applied formatting

--- main.tf
+++ main.tf
@@ -1,20 +1,20 @@
 resource "namecheap_domain_records" "lilja-rocks-ns" {
   domain = "lilja.rocks"
-  mode = "OVERWRITE"
+  mode   = "OVERWRITE"
 
   record {
     hostname = "blog"
-    type = "A"
-    address = "10.11.12.13"
+    type     = "A"
+    address  = "10.11.12.13"
   }
 }
 
 resource "namecheap_domain_records" "lilja-io-ns" {
   domain = var.namecheap_domain
-  mode = "OVERWRITE"
+  mode   = "OVERWRITE"
 
   nameservers = [
-            "amit.ns.cloudflare.com",
+    "amit.ns.cloudflare.com",
     "anna.ns.cloudflare.com"
   ]
 }
