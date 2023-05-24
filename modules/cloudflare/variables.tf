21c098daac77a18982f2fd54b583b77208b00b2b
Jonas Lilja <jonas@lilja.io>
2023-05-24 21:43:34 +0200

    Add workflow for planning cloudflare terraform

---
+++ modules/cloudflare/variables.tf
@@ -0,0 +1,30 @@
+variable "cloudflare_api_token" {
+  type        = string
+  description = "(Required) API token for authenticating with the cloudflare api."
+}
+
+variable "domain_name" {
+  description = "The cloudflare domain to configure."
+  type        = string
+  default     = "lilja.io"
+}
+
+variable "protonmail_verification" {
+  description = "The protonmail txt verification dns record."
+  type        = string
+}
+
+variable "protonmail_dkim1" {
+  description = "The protonmail CNAME dkim1 dns record."
+  type        = string
+}
+
+variable "protonmail_dkim2" {
+  description = "The protonmail CNAME dkim2 dns record."
+  type        = string
+}
+
+variable "protonmail_dkim3" {
+  description = "The protonmail CNAME dkim3 dns record."
+  type        = string
+}
