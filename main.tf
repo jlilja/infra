4150c25a8a0f1da5ebe89e0a6196f59866876253
Jonas Lilja <jonas@lilja.io>
2023-05-29 00:28:45 +0200

    Create module for namecheap setup

--- main.tf
+++ main.tf
@@ -6,12 +6,25 @@ terraform {
   }
 }
 
+locals {
+  domain_name = "lilja.io"
+}
+
 module "cloudflare_setup" {
   source                  = "./modules/cloudflare"
-  domain_name             = "lilja.io"
+  domain_name             = local.domain_name
   cloudflare_api_token    = var.cloudflare_api_token
   protonmail_verification = var.protonmail_verification
   protonmail_dkim1        = var.protonmail_dkim1
   protonmail_dkim2        = var.protonmail_dkim2
   protonmail_dkim3        = var.protonmail_dkim3
+}
+
+module "namecheap_setup" {
+  source               = "./modules/namecheap"
+  domain_name          = local.domain_name
+  cloudflare_api_token = var.cloudflare_api_token
+  user_name            = var.namecheap_user_name
+  api_user             = var.namecheap_api_user
+  api_key              = var.namecheap_api_key
 }
