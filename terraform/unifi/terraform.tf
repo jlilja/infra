b4934fa52297a3b25f01d760901526ab01bf9afe
Jonas Lilja <jonas@lilja.io>
2024-08-13 22:18:27 +0200

    Set credentials as variables. Rotated credentials.

--- terraform/unifi/terraform.tf
+++ terraform/unifi/terraform.tf
@@ -1,6 +1,6 @@
 provider "unifi" {
-  username       = "ar23LPM8bFza9HRM"                                                                                     # var.unifi_username
-  password       = "pB6.c7vhu_GU9FMNgjk_uXW_Pq-N_J2ku*QQYncq_HaYUp.HKHtPzuWj*iDYGsbqyXxabCC3g*6B!x36gyfeR-x7*GNM-U84nbWT" # var.unifi_password
+  username       = var.unifi_username # var.unifi_username
+  password       = var.unifi_password # var.unifi_password
   api_url        = local.api_url
   allow_insecure = local.insecure
 }
--- terraform/unifi/variables.tf
+++ terraform/unifi/variables.tf
@@ -1,9 +1,9 @@
-# variable "unifi_username" {
-#   type      = string
-#   sensitive = true
-# }
+variable "unifi_username" {
+  type      = string
+  sensitive = true
+}
 
-# variable "unifi_password" {
-#   type      = string
-#   sensitive = true
-# }
+variable "unifi_password" {
+  type      = string
+  sensitive = true
+}
