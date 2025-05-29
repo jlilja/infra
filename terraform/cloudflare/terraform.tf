9b5a306d6b481ca11f19ab331eb1a17d6f55f460
Jonas Lilja <jonas@lilja.io>
2025-05-29 12:16:20 +0200

    Add provider to the directory of the one calling the module

--- terraform/cloudflare/terraform.tf
+++ terraform/cloudflare/terraform.tf
@@ -4,4 +4,11 @@ terraform {
     key    = "infra"
     region = "eu-north-1"
   }
+
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.0"
+    }
+  }
 }
