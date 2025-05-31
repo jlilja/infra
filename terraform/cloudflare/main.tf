0473ef633ed93fa6f5122c74408c54e396c52913
Jonas Lilja <jonas@lilja.io>
2025-05-31 10:32:11 +0200

    Test new property pt 9

--- terraform/cloudflare/main.tf
+++ terraform/cloudflare/main.tf
@@ -65,3 +65,13 @@ moved {
   from = module.cloudflare_lilja_io_setup.cloudflare_record.txt_dmarc
   to   = module.cloudflare_lilja_io_setup.cloudflare_record.txt_records["_dmarc-v=DMARC1; p=quarantine"]
 }
+
+moved {
+  from = module.cloudflare_lilja_io_setup.cloudflare_record.mx_proton_mail["mail.protonmail.ch"]
+  to   = module.cloudflare_lilja_io_setup.cloudflare_record.mx_records["mail.protonmail.ch"]
+}
+
+moved {
+  from = module.cloudflare_lilja_io_setup.cloudflare_record.mx_proton_mail["mailsec.protonmail.ch"]
+  to   = module.cloudflare_lilja_io_setup.cloudflare_record.mx_records["mailsec.protonmail.ch"]
+}
