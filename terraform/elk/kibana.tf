a1d1566b0e1e69b5a66a83d755a08aa69423ffb6
Jonas Lilja <jonas@lilja.io>
2025-06-11 22:09:02 +0200

    Add configs for syslog and elk stack

---
+++ terraform/elk/kibana.tf
@@ -0,0 +1,8 @@
+resource "elasticstack_kibana_data_view" "syslog_view" {
+  data_view = {
+    name            = "Syslogs"
+    title           = "syslog-*.*.*"
+    time_field_name = "@timestamp"
+    namespaces      = ["infra"]
+  }
+}
