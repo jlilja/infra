5f46b7ff940b31eacc1cd9b3e355cf5d8bb87c32
Jonas Lilja <jonas@lilja.io>
2025-09-06 15:19:17 +0200

    Update port overrides. Cleanup old taskfile

--- terraform/unifi/devices.tf
+++ terraform/unifi/devices.tf
@@ -52,6 +52,13 @@ resource "unifi_device" "USW_24_G1" {
   }
 
   port_override {
+    number = 20
+    name   = unifi_port_profile.infra.name
+
+    port_profile_id = unifi_port_profile.infra.id
+  }
+
+  port_override {
     number = 22
     name   = unifi_port_profile.infra.name
 
