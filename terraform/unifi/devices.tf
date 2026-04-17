ef7419fc7a83134c749ec91907105df2d219ad42
Jonas Lilja <jonas@lilja.io>
2026-04-17 08:15:56 +0200

    Doco-cd work (#278)
    * Update documentation. Add playbook to populate files for doco. Add script to update file naming before moving to a zfs fs.
    
    * Add new vlan for ds418 management port.
    
    * Replace outdated provider. Use new ubiquiti-community/unifi provider
    
    * Move router to unifi and terraform managed.
    
    * Remove old ansible provisioning edgerouter firewall.
    
    * Add dococd for jameson
    
    * Add script for cleaning synology dirs

--- terraform/unifi/devices.tf
+++ terraform/unifi/devices.tf
@@ -1,8 +1,7 @@
 resource "unifi_device" "USW_24_G1" {
   name = local.USW_24_G1
 
-  site = unifi_site.default.name
-  mac  = "18:e8:29:b0:41:da"
+  mac = "18:e8:29:b0:41:da"
 
   port_override {
     number = 1
@@ -10,84 +9,58 @@ resource "unifi_device" "USW_24_G1" {
   }
 
   port_override {
-    number = 3
-    name   = unifi_port_profile.iot.name
-
-    port_profile_id = unifi_port_profile.iot.id
+    number          = 5
+    name            = module.ds418p-data.network_name
+    port_profile_id = module.ds418p-data.network_port_profile_id
   }
 
   port_override {
-    number = 5
-    name   = unifi_port_profile.ds418p.name
-
-    port_profile_id = unifi_port_profile.ds418p.id
+    number          = 7
+    name            = module.ds418p-mgmt.network_name
+    port_profile_id = module.ds418p-mgmt.network_port_profile_id
   }
 
   port_override {
-    number = 9
-    name   = unifi_port_profile.trusted.name
-
-    port_profile_id = unifi_port_profile.trusted.id
-  }
-
-  port_override {
-    number = 15
-    name   = unifi_port_profile.iot.name
-
-    port_profile_id = unifi_port_profile.iot.id
-  }
-
-  port_override {
-    number = 17
-    name   = unifi_port_profile.trusted.name
-
-    port_profile_id = unifi_port_profile.trusted.id
-  }
-
-  port_override {
-    number = 19
-    name   = unifi_port_profile.trusted.name
-
-    port_profile_id = unifi_port_profile.trusted.id
-  }
-
-  port_override {
-    number = 20
-    name   = unifi_port_profile.infra.name
-
-    port_profile_id = unifi_port_profile.infra.id
-  }
-
-  port_override {
-    number = 22
-    name   = unifi_port_profile.dns.name
-
-    port_profile_id = unifi_port_profile.dns.id
-  }
-
-  port_override {
-    number = 23
-    name   = "unifi-cloudkey" # Don't modify until ready
-  }
-
-  port_override {
-    number = 24
-    name   = unifi_port_profile.dns.name
-
-    port_profile_id = unifi_port_profile.dns.id
+    number          = 9
+    name            = module.trusted.network_name
+    port_profile_id = module.trusted.network_port_profile_id
   }
 }
 
 resource "unifi_device" "US_8_60W" {
   name = local.US_8_60W
 
-  site = unifi_site.default.name
-  mac  = "b4:fb:e4:8d:13:c9"
+  mac = "b4:fb:e4:8d:13:c9"
 }
 
 resource "unifi_device" "UAP_AC_Lite" {
   name = local.UAP_AC_Lite
 
-  site = unifi_site.default.name
-  mac  = "fc:ec:da:a6:14:31"
+  mac = "fc:ec:da:a6:14:31"
+}
+
+# resource "unifi_device" "UCK_G2_Plus" {
+#   name = local.UCK_G2_Plus
+
+#   mac = "18:e8:29:b2:d1:e9"
+# }
+
+# import {
+#   id = "18:e8:29:b2:d1:e9"
+#   to = unifi_device.UCK_G2_Plus
+# }
+
+# resource "unifi_device" "USG_Pro_4" {
+#   name = local.USG_Pro_4
+
+#   mac = "74:83:c2:74:3b:3b"
+# }
+
+resource "unifi_site" "default" {
+  description = "test"
+}
+
+import {
+  id = "default"
+  to = unifi_site.default
 }
