3a371e5d9b34a4bb6ed77b7afc95cbba6953669d
Jonas <jonas@lilja.io>
2026-08-31 21:47:36 +0200

    Reduce code repetition with loops. Added Plex

--- apps/pihole/terraform/lists.tf
+++ apps/pihole/terraform/lists.tf
@@ -1,20 +1,28 @@
-resource "pihole_list" "hagezi_dns_blocklist_multi_pro" {
-  address = "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/adblock/pro.txt"
-  type    = "block"
-  enabled = true
-  comment = "https://github.com/hagezi/dns-blocklists#ledger-multi-pro---extended-protection-recommended-"
+locals {
+  block_lists = {
+    adblock_pro = {
+      address = "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/adblock/pro.txt",
+      enabled = true,
+      comment = "https://github.com/hagezi/dns-blocklists#ledger-multi-pro---extended-protection-recommended-"
+    },
+    ablock_tif = {
+      address = "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/adblock/tif.txt",
+      enabled = true,
+      comment = "https://github.com/hagezi/dns-blocklists#closed_lock_with_key-threat-intelligence-feeds---increases-security-significantly-recommended-"
+    },
+    adblock_multi_pro = {
+      address = "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/adblock/pro.plus.txt",
+      enabled = true,
+      comment = "https://github.com/hagezi/dns-blocklists#orange_book-multi-pro---maximum-protection-"
+    },
+  }
 }
 
-resource "pihole_list" "hagezi_dns_blocklist_tif" {
-  address = "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/adblock/tif.txt"
-  type    = "block"
-  enabled = false
-  comment = "https://github.com/hagezi/dns-blocklists#closed_lock_with_key-threat-intelligence-feeds---increases-security-significantly-recommended-"
-}
+resource "pihole_list" "block_list" {
+  for_each = local.block_lists
 
-resource "pihole_list" "hagezi_dns_blocklist_multi_pro_maximum_prot" {
-  address = "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/adblock/pro.plus.txt"
+  address = each.value.address
   type    = "block"
-  enabled = false
-  comment = "https://github.com/hagezi/dns-blocklists#orange_book-multi-pro---maximum-protection-"
+  enabled = each.value.enabled
+  comment = each.value.comment
 }
